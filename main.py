28ec9324e83530eac5d5afd9bf531d9db0dde717
Summaria-tech <nomphueng2hotmail.com@gmail.com>
2026-02-20 18:45:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ async def check_and_send(bot):
     
     if res.status_code == 200:
         games = res.json()
-        for game in reversed(games[:50]):
+        for game in reversed(games[:100]):
             game_id = str(game['id'])
             if game_id not in sent_ids:
                 genre_list = get_detailed_genres(game)
@@ -120,3 +120,4 @@ async def on_ready():
 if __name__ == "__main__":
     if TOKEN and CHANNEL_ID:
         bot.run(TOKEN)
+
